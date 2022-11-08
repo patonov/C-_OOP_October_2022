7a84bcada363660f4d446b2c35b416a318ab995a
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-11-08 23:07:34 +0200

    Add files via upload

---
+++ NavalVessels-Skeleton/NavalVessels/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace NavalVessels
+{
+    using System;
+    using System.Globalization;
+    using System.Threading;
+    using Core;
+    using Core.Contracts;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+            IEngine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
