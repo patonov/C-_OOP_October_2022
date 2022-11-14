8632d04dccc22a275893c8d4635ea4e9457ae19f
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-11-14 20:27:51 +0200

    Add files via upload

---
+++ Telephony/Telephony/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Telephony
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] phones = Console.ReadLine().Split();
+            string[] urls = Console.ReadLine().Split();
+
+            Smartphone smartphone = new Smartphone();
+            StationaryPhone stationaryPhone = new StationaryPhone();
+
+            foreach (var number in phones)
+            {
+                try
+                {
+                    string result = number.Length == 10 ? smartphone.Call(number) : stationaryPhone.Call(number);
+
+                    Console.WriteLine(result);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            foreach (var url in urls)
+            {
+                try
+                {
+                    var result = smartphone.Browse(url);
+
+                    Console.WriteLine(result);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Telephony/Telephony/StationaryPhone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telephony
+{
+    public class StationaryPhone : Phone
+    {
+        public override string Call(string number)
+        {
+            Validator.ThrowIfNumberIsInvalid(number);
+
+            return $"Dialing... {number}";
+        }
+    }
+}
