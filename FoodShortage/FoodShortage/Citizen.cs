dc41d7fb383dbbbedb7bcb80a1a6de6b77d41e24
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2022-11-22 01:19:19 +0200

    Add files via upload

---
+++ FoodShortage/FoodShortage/Citizen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IBirthable, IIdentifiable
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.BirthDate = birthdate;
+        }
+
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string BirthDate { get; private set; }
+
+        public string Id { get; private set; }
+
+        public int Food { get; private set; }
+
+        public void BuyFood()
+        {
+            this.Food += 10;
+        }
+    }
+}
---
+++ FoodShortage/FoodShortage/IPerson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PersonInfo
+{
+    public interface IPerson : IBuyer
+    {
+        string Name { get; }
+
+        int Age { get; }
+        
+    }
+}
