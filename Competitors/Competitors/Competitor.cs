c2798938765ec40765a9a6dd651ef4a17a1945e2
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2025-01-21 19:49:00 +0200

    Add files via upload

---
+++ Competitors/Competitors/Competitor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitors
+{
+    public class Competitor<T> : IEnumerable<T>, IComparable<Competitor<T>>
+    {
+        private string name;
+        private int age;
+        private List<T> scores;
+
+        public Competitor(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.scores = new List<T>();
+        }
+
+        public string Name { get; set; }
+
+        public int Age 
+        { 
+            get => this.age;
+            set
+            {
+                if (value < 10) 
+                {
+                    throw new ArgumentException("Age cannot be less than 10.");
+                }
+                this.age = value;
+            }
+        }
+
+        public List<T> Scores { get; set; }
+
+        public void Add(T score)
+        { 
+            this.scores.Add(score);
+        }
+
+        public int CountCompetitions()
+        { 
+            return this.scores.Count;
+        }
+
+        public T ChangeLastScore(T newScore)
+        { 
+            T lastScore = this.scores[this.scores.Count - 1];
+            this.scores[this.scores.Count - 1] = newScore;
+            return lastScore;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.scores.Count; i++)
+            { 
+                yield return this.scores[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public int CompareTo(Competitor<T> other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+            if (result == 0)
+            { 
+                result = this.Age.CompareTo(other.Age);
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Competitors/Competitors/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Competitors
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Competitor<int> IvanTheWiner = new Competitor<int>("Ivan", 16);
+            Competitor<int> Vanya = new Competitor<int>("Vanya", 17);
+            IvanTheWiner.Add(6);
+            IvanTheWiner.Add(2);
+            IvanTheWiner.Add(5);
+            IvanTheWiner.Add(4);
+            Console.WriteLine(IvanTheWiner.CompareTo(Vanya));
+
+
+
+
+        }
+    }
+}
